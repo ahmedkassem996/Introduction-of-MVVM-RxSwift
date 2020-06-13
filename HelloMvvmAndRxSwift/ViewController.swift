e1849179ddc9a6724f44ea6d97ffbadaaab9e069
Ahmed Kassem <ahmedkassem996@gmail.com>
2020-06-13 18:47:47 +0200

    initial commit

--- HelloMvvmAndRxSwift/ViewController.swift
+++ HelloMvvmAndRxSwift/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var userNameTF: UITextField!
+  @IBOutlet weak var passwordTF: UITextField!
+  @IBOutlet weak var loginBtn: UIButton!
+  
+  private let loginViewModel = LoginViewModel()
+  private let disposeBag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    userNameTF.becomeFirstResponder()
+    
+    userNameTF.rx.text.map { $0 ?? "" } .bind(to: loginViewModel.userNameTextPublishSubject).disposed(by: disposeBag)
+    passwordTF.rx.text.map { $0 ?? "" } .bind(to: loginViewModel.passwordTextPublishSubject).disposed(by: disposeBag)
+    
+    loginViewModel.isValid().bind(to: loginBtn.rx.isEnabled).disposed(by: disposeBag)
+    loginViewModel.isValid().map { $0 ? 1 : 0.1 }.bind(to: loginBtn.rx.alpha).disposed(by: disposeBag)
   }
 
 
+  @IBAction func tappedLoginBtn(_ sender: UIButton) {
+    print("Tapped login button")
+  }
 }
 
+class LoginViewModel {
+  let userNameTextPublishSubject = PublishSubject<String>()
+  let passwordTextPublishSubject = PublishSubject<String>()
+  
+  func isValid() -> Observable<Bool> {
+    return Observable.combineLatest(userNameTextPublishSubject.asObservable().startWith(""), passwordTextPublishSubject.asObservable().startWith("")).map { username, password in
+      return username.count > 3 && password.count > 3
+    }.startWith(false)
+  }
+}
---
+++ Pods/Target Support Files/Pods-HelloMvvmAndRxSwift/Pods-HelloMvvmAndRxSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloMvvmAndRxSwift : NSObject
+@end
+@implementation PodsDummy_Pods_HelloMvvmAndRxSwift
+@end
